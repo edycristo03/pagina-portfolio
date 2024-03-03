dfd4580eefc2730fdfb6495e92e9221ea0313301
edycristo03 <edmilsonivair@gmail.com>
2024-03-03 20:49:17 -0300

    Alteração Raw

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,7 @@
 
 async function fetchProfileData() {
 
-    const url = 'https://raw.githubusercontent.com/edycristo03/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/edycristo03/PAGINA-DE-PORTIFOLIO/master/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
